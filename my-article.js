2fcf7a3a8d7683a42582947277f6d071348140f3
ivinogradov <ilya.vngrdv@gmail.com>
2019-05-03 15:13:52 -0500

    Create article template and properties
    Summary:
    
    (c) Ilya Vinogradov, 2019

--- my-article.js
+++ my-article.js
@@ -15,15 +15,31 @@ class MyArticle extends PolymerElement {
         :host {
           display: block;
         }
+        .article{
+          display: block;
+          width: 25%;
+          background-color: #fff;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+          text-align: center;
+          padding: 1%;
+          margin: 1%;
+        }
       </style>
-      <h2>Hello [[name]]!</h2>
+      <div class="article">
+        <h2>[[title]]</h2>
+        <p>[[content]]</p>
+      </div>
     `;
   }
   static get properties() {
     return {
-      name: {
+      title: {
         type: String,
-        value: 'World',
+        value: 'Title',
+      },
+      content: {
+        type: String,
+        value: 'Content'
       }
     };
   }
